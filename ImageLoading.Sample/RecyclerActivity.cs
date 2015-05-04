b530b940f99b14abef7de4f5a5a786304b9cf2b3
Peter Luptak <petolup@gmail.com>
2015-05-04 21:32:00 +0200

    Add recyclerview sample

---
+++ ImageLoading.Sample/RecyclerActivity.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using System.Collections.Generic;
+using Android.Support.V4.View;
+using FFImageLoading;
+using FFImageLoading.Views;
+using Android.Content.Res;
+using Android.Support.V4.App;
+using Android.App;
+using Android.Support.V7.Widget;
+
+namespace ImageLoading.Sample
+{
+    [Android.App.Activity(Label = "ImageLoading - Sample", MainLauncher = true, Icon = "@drawable/icon")]
+    public class RecyclerActivity : Activity
+    {
+        public const string POSITION = "position";
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.list);
+
+            var list = FindViewById<RecyclerView>(Resource.Id.venueList);
+            var adapter = new PictureAdapter(this);
+            adapter.Items = Config.Images;
+
+            RecyclerView.LayoutManager layoutManagerDelegate = new LinearLayoutManager(this);
+            list.SetLayoutManager(layoutManagerDelegate);
+            list.SetAdapter(adapter);
+
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.options, menu);
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+                case Resource.Id.btnSwipe:
+                    StartActivity(typeof(MainActivity));
+                    return true;
+                case Resource.Id.btnRecycler:
+                    StartActivity(typeof(RecyclerActivity));
+                    return true;
+                default:
+                    return base.OnOptionsItemSelected(item);
+            }
+        }
+    }
+}
+
